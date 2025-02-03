b4aabe6352d9d9099e6a047edf383d5a4b167497
ankitgaur <gaur.ankit2007@gmail.com>
2025-02-02 21:28:03 -0700

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,32 @@
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var tfidfSearch: TFIDFSearch
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val faqModel = FAQModel(this)
+        tfidfSearch = TFIDFSearch(faqModel.faqs)
+
+        val editTextQuery = findViewById<EditText>(R.id.editTextQuery)
+        val buttonSearch = findViewById<Button>(R.id.buttonSearch)
+        val textViewResults = findViewById<TextView>(R.id.textViewResults)
+
+        buttonSearch.setOnClickListener {
+            val query = editTextQuery.text.toString()
+            if (query.isNotEmpty()) {
+                val answer = tfidfSearch.search(query)
+                textViewResults.text = answer
+            } else {
+                textViewResults.text = "Enter a search question"
+            }
+        }
+    }
+}
